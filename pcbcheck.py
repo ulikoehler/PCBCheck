3ba9bf1a522ff294fd3b46d279cea7ab8bdb38c5
Uli Köhler <ulikoehler@online.de>
2014-12-07 19:18:00 +0100

    Added automated board size computation

--- pcbcheck.py
+++ pcbcheck.py
@@ -78,6 +78,88 @@ allowedLayerNotes.update({
     "Silk screen top": ["TopSilk"],
 })
 
+#Gerber aperture
+# id: The aperture identifier, e.g. D11
+# type: "C"/"R"
+# diameter: float, with implicit units
+Aperture = namedtuple("Aperture", ["id", "type", "diameter"])
+
+def parseGerberApertures(lines):
+    "From a list of gerber lines, parse all embedded apertures"
+    apertureRegex = re.compile(r"%AD(D\d+)([CR]),(\d+\.\d+)\*%")
+    apertures = []
+    #Find lines defining apertures
+    for line in lines:
+        if apertureRegex.match(line):
+            match = apertureRegex.match(line)
+            apertures.append(Aperture(match.group(1), match.group(2), float(match.group(3))))
+    return apertures
+
+def findAperture(apertures, identifier):
+    "Find an aperture in a list of apertures (returns None if not found)"
+    for aperture in apertures:
+        if aperture.id == identifier: return aperture
+    return None
+
+def checkBoardOutline(self, filepath):
+    filename = os.path.basename(filepath)
+    #Basic gerber checks
+    checkGerberFile(self, filepath)
+    #Compute board outline
+    millLines = readFileLines(filepath)
+    #We can only interpret the file if coordinates are absolute
+    if not "G90*" in millLines:
+        print (yellow("Mill coordinates in %s don't seem to be absolute (G90 missing!)" % filename))
+        return
+    #Determine coordinate units
+    unit = None
+    if "%MOIN*%" in millLines:
+        unit = "in"
+    elif "%MOMM*%" in millLines:
+        unit = "mm"
+    if unit is None: #Neither inch nor mm found
+        print (yellow("Could not find coordinate units (mm/in) in %s" % filename))
+        return
+    #Parse the aperture list
+    apertures = parseGerberApertures(millLines)
+    selectApertureRegex = re.compile(r"(D\d+)\*")
+    move2DRegex = re.compile(r"X(\d+)Y(\d+)D(\d)\*") #Move (D2) or draw (D1)
+    move1DRegex = re.compile(r"([XY])(\d+)D(\d)\*") #With only one coordinate
+    #Try to interpret gerber file
+    minCoords = (sys.maxsize, sys.maxsize)
+    maxCoords = (0, 0)
+    lastCoords = (0, 0)
+    currentAperture = None
+    for line in millLines:
+        if selectApertureRegex.match(line):
+            apertureCode = selectApertureRegex.match(line).group(1)
+            currentAperture = findAperture(apertures, apertureCode)
+        elif move2DRegex.match(line):
+            match = move2DRegex.match(line)
+            x = int(match.group(1)) / 1000.0
+            y = int(match.group(2)) / 1000.0
+        elif move1DRegex.match(line):
+            match = move1DRegex.match(line)
+            if match.group(1) == "X":
+                x = int(match.group(2)) / 1000.0
+                y = lastCoords[1]
+            elif match.group(1) == "Y":
+                x = lastCoords[0]
+                y = int(match.group(2)) / 1000.0
+            else: raise Exception("Internal error: Invalid coordinate type: %s" % match.group(1))
+        else: continue
+        #Compute min/max coordinates
+        lastCoords = (x, y)
+        minCoords = (min(minCoords[0], lastCoords[0]), min(minCoords[1], lastCoords[1]))
+        maxCoords = (max(maxCoords[0], lastCoords[0]), max(maxCoords[1], lastCoords[1]))
+    #Compute board size (minimum enclosing rectangle)
+    boardSize = (maxCoords[0] - minCoords[0], maxCoords[1] - minCoords[1])
+    #Print info
+    print (black("\tGerber offset: %s" % str(minCoords)))
+    print (black("\tBoard size (minimum rectangle): %.1f %s x %.1f %s" % \
+            (boardSize[0], unit, boardSize[1], unit)))
+
+
 def checkGerberFile(self, filepath):
     """
     Check if the given file is a RS-274X gerber file
@@ -123,7 +205,7 @@ def checkFile(directory, expectedFile, projectName):
     filename = projectName + expectedFile.extension
     filepath = os.path.join(directory, filename)
     if os.path.isfile(filepath):
-        print green("Found production data %s" % filename)
+        print green("Found %s data %s" % (expectedFile.format, filename))
         if expectedFile.checkFN is not None:
             expectedFile.checkFN(expectedFile, filepath)
     else:
@@ -131,18 +213,18 @@ def checkFile(directory, expectedFile, projectName):
         return None
     return filename
 
-ExpectedFile = namedtuple('ExpectedFile', ['extension', 'name', 'checkFN'])
+ExpectedFile = namedtuple('ExpectedFile', ['extension', 'name', 'format', 'checkFN'])
 expectedFiles = [
     #http://www.multi-circuit-boards.eu/support/leiterplatten-daten/gerber-daten.html
-    ExpectedFile(".top", "Top copper layer", checkGerberFile),
-    ExpectedFile(".bot", "Bottom copper layer", checkGerberFile),
-    ExpectedFile(".smt", "Solder mask top", checkGerberFile),
-    ExpectedFile(".smb", "Solder mask bottom", checkGerberFile),
-    ExpectedFile(".plt", "Silk screen top", checkGerberFile),
-    ExpectedFile(".mil", "Board outline", checkGerberFile),
+    ExpectedFile(".top", "Top copper layer", "RS-274X", checkGerberFile),
+    ExpectedFile(".bot", "Bottom copper layer", "RS-274X", checkGerberFile),
+    ExpectedFile(".smt", "Solder mask top", "RS-274X", checkGerberFile),
+    ExpectedFile(".smb", "Solder mask bottom", "RS-274X", checkGerberFile),
+    ExpectedFile(".plt", "Silk screen top", "RS-274X", checkGerberFile),
+    ExpectedFile(".mil", "Board outline", "RS-274X", checkBoardOutline),
     #Drilling
-    ExpectedFile(".pth", "Plated through holes", checkExcellonMetric),
-    ExpectedFile(".npth", "Non-plated through holes", checkExcellonMetric),
+    ExpectedFile(".pth", "Plated through holes", "Excellon", checkExcellonMetric),
+    ExpectedFile(".npth", "Non-plated through holes", "Excellon", checkExcellonMetric),
 ]
 
 if __name__ == "__main__":
@@ -155,7 +237,7 @@ if __name__ == "__main__":
     #Perform check
     files = os.listdir(args.directory)
     projectName = extractProjectPrefix(files)
-    print black("Found project name %s" % projectName)
+    print black("Project name: %s" % projectName)
     checkedFiles = [checkFile(args.directory, f, projectName) for f in expectedFiles]
     unknownFiles = set(files) - set(checkedFiles)
     if unknownFiles:
